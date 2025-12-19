4b20ed7073191699ed0fd4b2ddc17a585be38995
Revanth Singothu <revanthsingothu@gmail.com>
2025-12-19 07:50:29 +0000

    rm: nuked convex

--- convex/lib/code.ts
+++
@@ -1,8 +0,0 @@
-export function generateBoardCode(): string {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let code = "";
-  for (let i = 0; i < 6; i++) {
-    code += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return code;
-}
